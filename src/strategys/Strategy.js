7d02083d23908f083b79c6069ed389061440b688
Hugo Azevedo <hugo.soares@outlook.com>
2018-02-04 15:41:21 -0200

    estratégias adicionadas no local correto

--- src/strategys/Strategy.js
+++
@@ -1,9 +0,0 @@
-class Strategy {
-
-    constructor(enemy) {
-        if (!enemy)
-            throw new Error('Um inimigo é obrigatório para a criação de uma estratégia');
-        this.enemy = enemy;
-    }
-
-}
